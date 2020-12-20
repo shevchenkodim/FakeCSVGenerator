f0afedde20f16b5fcbb4927e81efb8f9346bb3c6
Dmytro <dimashevchenko10021999@gmail.com>
2020-12-20 22:31:06 +0200

    STATIC_ROOT

--- FakeCSV/settings.py
+++ FakeCSV/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 # STATICFILES_DIRS = [STATIC_DIR]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
